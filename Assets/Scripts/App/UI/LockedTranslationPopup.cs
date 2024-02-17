40865ce82db7524f3ed21a7a72617c7fbf2af794
KAMEN-NiKNexT <106864035+KAMEN-NiKNexT@users.noreply.github.com>
2024-02-17 23:14:28 +0300

    UI fix

--- Assets/Scripts/App/UI/LockedTranslationPopup.cs
+++ Assets/Scripts/App/UI/LockedTranslationPopup.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Kamen.UI;
+//using CatTranslator.SaveData;
+using UnityEngine.UI;
 
 namespace CatTranslator.UI
 {
@@ -9,13 +11,35 @@ namespace CatTranslator.UI
     {
         #region Variables
 
-
+        [SerializeField] private Button _subscribeButton;
+        [SerializeField] private TranslatorScreen _translatorScreen;
 
         #endregion
 
         #region Properties
 
 
+
+        #endregion
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _subscribeButton.onClick.AddListener(CallSubscribe);
+        }
+
+        #region Control  Methods
+
+        public void CallSubscribe()
+        {
+            PurchasesListener.Instance.PurchaseSubscription("test", ShowResult);
+        }
+        private void ShowResult()
+        {
+            PopupManager.Instance.Hide("LockedTranslationPopup");
+            _translatorScreen.CallShowResalt();
+        }
 
         #endregion
     }
